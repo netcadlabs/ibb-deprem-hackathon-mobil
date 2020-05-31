6e6ddc50ff40b4ad4a9b45e7f6d325c979cc886d
agtokty <agitoktay@gmail.com>
2020-05-31 15:38:44 +0300

    version 0.0.3 & platform url ve tenantid değeri düzeltme

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,10 +3,10 @@ class Constants {
   static const String MQTT_HOST = "10.10.10.6";
   static const String GATEWAY_CREDENTIAL = "-----";
 
-//  static const String TENANT_ID = "0e3d0460-a0eb-11ea-aaa5-859c6e11d8c4";
-//  static const String NDU_HOST = "https://smartapp.netcad.com";
+  static const String TENANT_ID = "0e3d0460-a0eb-11ea-aaa5-859c6e11d8c4";
+  static const String NDU_HOST = "https://smartapp.netcad.com";
 
 // Local Test
-  static const String TENANT_ID = "c91b9940-3836-11ea-89f2-e7b083ab17e4";
-  static const String NDU_HOST = "http://192.168.1.2:8080";
+//  static const String TENANT_ID = "c91b9940-3836-11ea-89f2-e7b083ab17e4";
+//  static const String NDU_HOST = "http://192.168.1.2:8080";
 }
